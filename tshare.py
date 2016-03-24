3144b4266cf1d2ab96621134c6fdd4f9dfc17896
liangxh <sekhou64@hotmail.com>
2016-03-24 15:11:13 +0800

    test for theano.shared for gpu

---
+++ tshare.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import theano
+
+def main():
+	a = np.zeros((54912, 128)).astype(theano.config.floatX)
+	ta = theano.shared(a, borrow=True)
+	
+	return ta
+
+if __name__ == '__main__':
+	main()
