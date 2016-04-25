072fd9536234be0bdacf13d9a4ff31299bbc6650
liangxh <sekhou64@hotmail.com>
2016-04-25 21:19:00 +0800

    wvbuilder test

--- word2vec_builder.py
+++ word2vec_builder.py
@@ -53,7 +53,7 @@ class DBTextIterator:
 		self.con = db.connect()
 		self.cur = self.con.cursor()
 
-		self.cur.execute('SELECT COUNT(*) FROM microblogs LIMIT 3')
+		self.cur.execute('SELECT COUNT(*) FROM microblog')
 		n_text = self.cur.fetchone()[0]
 		print >> sys.stderr, 'Totally %d text, executing SELECT text FROM microblogs...'%(n_text), 
 	
