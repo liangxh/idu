4fa390b92fd6e95447e931d1340d71d72d0869d0
liangxh <sekhou64@hotmail.com>
2016-05-05 08:18:20 +0800

    simtext init

---
+++ simtext.py
@@ -0,0 +1,120 @@
+#! /usr/bin/env python
+#-*- coding: utf-8 -*-
+'''
+Author: Xihao Liang
+Created: 2016.04.25
+'''
+
+import os
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+import cPickle
+import Levenshtein
+import numpy as np
+
+import datica
+from utils import progbar
+
+
+def jacob(t1, t2):
+	return - len(set(t1).intersection(set(t2)))
+
+def sim_jacob():	
+	tid =  int(sys.argv[1])
+
+	phrases = open('data/eid.txt', 'r').read().decode('utf8').split('\n')
+
+	n_emo = 90
+	x = []
+	y = []
+	for eid in range(n_emo):
+		seqs = cPickle.load(open('data/dataset/unigram/%d.pkl'%(eid), 'r'))
+		x.extend(seqs)
+		y.extend([eid for i in range(len(seqs))])
+
+	func_distance = jacob
+
+	x_target = x[tid]
+	res = []
+	pbar = progbar.start(len(x))
+	for i, xi in enumerate(x):
+		res.append((i, func_distance(xi, x_target)))
+		pbar.update(i + 1)
+	pbar.finish()
+
+	res = sorted(res, key = lambda k:k[1])
+	
+	for i in range(10):
+		xid, v = res[i]
+		print '%d. (%d) [%s] %s'%(i, v, phrases[y[xid]], ''.join(x[xid]))	
+
+def sim_ED():
+	tid =  int(sys.argv[1])
+
+	phrases = open('data/eid.txt', 'r').read().decode('utf8').split('\n')
+
+	n_emo = 90
+	x = []
+	y = []
+	for eid in range(n_emo):
+		seqs = cPickle.load(open('data/dataset/unigram/%d.pkl'%(eid), 'r'))
+		for seq in seqs:
+			x.append(u''.join(seq))
+		y.extend([eid for i in range(len(seqs))])
+
+	func_distance = Levenshtein.distance
+	
+	x_target = x[tid]
+	bias = len(x_target)
+	res = []
+	pbar = progbar.start(len(x))
+	for i, xi in enumerate(x):
+		res.append((i, func_distance(xi, x_target) - bias))
+		pbar.update(i + 1)
+	pbar.finish()
+
+	res = sorted(res, key = lambda k:k[1])
+	
+	for i in range(10):
+		xid, v = res[i]
+		print '%d. (%d) [%s] %s'%(i, v, phrases[y[xid]], ''.join(x[xid]))
+
+def sim_ED_batch():
+	n_emo = 90
+
+	dataset = datica.load_data('data/dataset/unigram/', n_emo)
+	train, valid, test = dataset
+	train_x, train_y = train
+	test_x, test_y = test
+
+	train_x = [u''.join(seq) for seq in train_x]
+	test_x = [u''.join(seq) for seq in test_x]
+
+	n_test = len(test_x)
+	n_train = len(train_x)
+
+	records = []
+
+	pbar = progbar.start(n_test * n_train)
+	l = 0
+	for i in range(n_test):
+		target_x = test_x[i]
+		target_y = test_y[i]
+		bias = len(target_x)
+		
+		record = []
+		for xi, yi in zip(train_x, train_y):
+			record.append((yi, Levenshtein.distance(target_x, xi)))
+			l += 1			
+			pbar.update(l)
+
+		records.append((target_y, bias, record))
+	
+	pbar.finish()
+
+	cPickle.dump(records, open('data/simrecord_ED.pkl', 'w'))	 
+
+if __name__ == '__main__':
+	sim_ED_batch()
