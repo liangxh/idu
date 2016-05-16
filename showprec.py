c1f375229de14d618db57c56e661d709a30c20c4
liangxh <sekhou64@hotmail.com>
2016-05-16 17:12:46 +0800

    showprec.py init

---
+++ showprec.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Author: Xihao Liang
+Created: 2016.05.16
+'''
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+import cPickle
+from optparse import OptionParser
+
+def main():
+	optparser = OptionParser()
+	optparser.add_option('-i', '--input', action = 'store', dest = 'input', type = 'str')
+	optparser.add_option('-n', '--topN', action = 'store', dest = 'topN', type = 'int', default = 10)
+	opts, args = optparser.parse_args()
+
+	prec = cPickle.load(open('data/dataset/test/%s_prec.pkl'%(opts.input), 'r'))
+	print '  '.join(['(%d)%.4f'%(i + 1, p[i]) for i in range(opts.topN)])
+
+if __name__ == '__main__':
+	main()
