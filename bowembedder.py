01564192bbb75f84ef312f9870f566dc1bb46cc9
liangxh <sekhou64@hotmail.com>
2016-05-16 21:31:32 +0800

    bow init

---
+++ bowembedder.py
@@ -0,0 +1,55 @@
+#! /usr/env/python
+# -*- coding: utf-8 -*-
+'''
+Author: Xihao Liang
+Created: 2016.03.25
+'''
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+import cPickle
+
+import numpy as np
+import theano
+import copy
+
+class BowEmbedder:
+	def __init__(self, Widx = None):
+		self.Widx = Widx
+		self.N = len(self.Widx)	
+
+	def embed(self, seq):
+		'''
+		return a sequence of tokens into a list of vectors
+		'''
+		vec = np.zeros(self.N)
+		
+		for t in seq:
+			if self.Widx.has_key(t):
+				vec[self.Widx[t]] += 1.
+
+		vec_sum = vec.sum()
+		if not vec_sum == 0.
+			vec /= vec_sum
+
+		return vec
+
+	def dump(self, fname):
+		'''
+		save the class/model
+		'''
+		cPickle.dump(self.Widx, open(fname, 'w'))
+
+	@classmethod
+	def load(self, fname):
+		'''
+		load a trained class/model
+		'''
+		Widx = cPickle.load(open(fname, 'r'))
+		return BowEmbedder(Widx, Wemb)
+
+if __name__ == '__main__':
+	pass
+		
+		
---
+++ wemb_tf.py
@@ -0,0 +1,49 @@
+#! /usr/env/python
+# -*- coding: utf-8 -*-
+'''
+Author: Xihao Liang
+Created: 2016.03.25
+'''
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+import cPickle
+
+import numpy as np
+import theano
+
+def build(seqs, N):
+	tf = {}
+
+	for seq in seqs:
+		for t in seq:
+			if tf.has_key(t):
+				tf[t] += 1
+			else:	
+				tf[t] = 1.
+
+	tf = sorted(tf.items(), key = lambda k: -k[1])
+	
+	nums = set('0123456789')
+	Widx = {}
+	idx = 0
+	for t, f in tf:
+		if not t in nums:
+			Widx[t]	= idx	
+			idx += 1
+			if idx == N:
+				break
+	
+	return Widx
+
+def prepare():
+	import datica
+	train, valid, test = datica.load_unigram(90, valid_rate = 0.)
+
+	x, y = train
+	Widx = build(x, 2000)
+	cPickle.dump(Widx, open('data/dataset/model/bow2000.pkl', 'w'))
+
+if __name__ == '__main__':
+	prepare()
