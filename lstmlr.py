d0fd9e32e36b52e62b0a4ccc82e1d46f37b8d290
liangxh <sekhou64@hotmail.com>
2016-05-20 12:13:36 +0800

    just update

--- lstmlr.py
+++ lstmlr.py
@@ -12,28 +12,25 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 import time
 import cPickle
+from collections import OrderedDict
+from optparse import OptionParser
 
 import numpy as np
 import theano
 import theano.tensor as T
-import theano.config.floatX  as floatX
-from collections import OrderedDict
+from theano import config.floatX as floatX
+from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
-from utils import lstmtool
 from utils.logger import Logger
-
-from optparse import OptionParser
-
 logger = Logger()
 
-from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
 # Set the random number generators' seeds for consistency
 SEED = 123 
-numpy.random.seed(SEED)
+np.random.seed(SEED)
 
 ###################### Shortcut ######################################
-def numpy_floatX(data):
+def np_floatX(data):
 	return np.asarray(data, dtype = floatX)
 
 def _p(pp, name):
@@ -61,10 +58,10 @@ def get_minibatches_idx(n, minibatch_size, shuffle=False):
 	'''
 	get batches of idx for range(1, n) and shuffle if needed
 	'''
-	idx_list = numpy.arange(n, dtype="int32")
+	idx_list = np.arange(n, dtype="int32")
 
 	if shuffle:
-		numpy.random.shuffle(idx_list)
+		np.random.shuffle(idx_list)
 
 	minibatches = []
 	minibatch_start = 0
@@ -79,7 +76,7 @@ def get_minibatches_idx(n, minibatch_size, shuffle=False):
 	return zip(range(len(minibatches)), minibatches)
 
 def load_params(path, params):
-	pp = numpy.load(path)
+	pp = np.load(path)
 	for kk, vv in params.iteritems():
 		if kk not in pp:
 			raise Warning('%s is not in the archive' % kk)
@@ -103,7 +100,7 @@ def dropout_layer(state_before, use_noise, trng):
 	return a dropout layer
 	$state_before refers to x, rename it later maybe
 	'''
-	proj = tensor.switch(
+	proj = T.switch(
 			use_noise,
 			(state_before
 				* trng.binomial(state_before.shape, p = 0.5, n = 1, dtype = state_before.dtype)),
@@ -111,73 +108,31 @@ def dropout_layer(state_before, use_noise, trng):
 		)
 	return proj
 
-def param_init_lstm(options, params, prefix='lstm'):
-	N = 4 # input~/input/output/forget gate
+def pred_error(f_pred, prepare_data, data, iterator, verbose=False):
+	'''
+	Just compute the error
+	'''
+	valid_err = 0
+	for _, valid_index in iterator:
+		x, mask, y = prepare_data(
+					[data[0][t] for t in valid_index],
+					np.array(data[1])[valid_index],
+					maxlen = None
+				)
+		preds = f_pred(x, mask)
+		targets = np.array(data[1])[valid_index]
+		valid_err += (preds == targets).sum()
 
-	W = np.concatenate([ortho_weight(options['dim_proj']) for i in range(N)], axis=1)
-	params[_p(prefix, 'W')] = W
+	valid_err = 1. - np_floatX(valid_err) / len(data[0])
 
-	U = np.concatenate([ortho_weight(options['dim_proj']) for i in range(N)], axis=1)
-	params[_p(prefix, 'U')] = U
+	return valid_err
 
-	b = np.zeros((N * options['dim_proj'],))
-	params[_p(prefix, 'b')] = b.astype(floatX)
-
-	return params
-
-def lstm_layer(tparams, state_below, options, prefix='lstm', mask=None):
-	nsteps = state_below.shape[0]
-	if state_below.ndim == 3:
-		n_samples = state_below.shape[1]
-	else:
-		n_samples = 1
-
-	assert mask is not None
-
-	def _slice(_x, n, dim):
-		if _x.ndim == 3:
-			return _x[:, :, n * dim:(n + 1) * dim]
-		return _x[:, n * dim:(n + 1) * dim]
-
-	def _step(m_, x_, h_, c_):
-		preact = tensor.dot(h_, tparams[_p(prefix, 'U')])
-		preact += x_
-
-		i = tensor.nnet.sigmoid(_slice(preact, 0, options['dim_proj']))
-		f = tensor.nnet.sigmoid(_slice(preact, 1, options['dim_proj']))
-		o = tensor.nnet.sigmoid(_slice(preact, 2, options['dim_proj']))
-		c = tensor.tanh(_slice(preact, 3, options['dim_proj']))
-
-		c = f * c_ + i * c
-		c = m_[:, None] * c + (1. - m_)[:, None] * c_
-
-		h = o * tensor.tanh(c)
-		h = m_[:, None] * h + (1. - m_)[:, None] * h_
-
-		return h, c
-
-	state_below = (tensor.dot(state_below, tparams[_p(prefix, 'W')]) +
-				   tparams[_p(prefix, 'b')])
-
-	dim_proj = options['dim_proj']
-	rval, updates = theano.scan(
-				_step,
-				sequences = [mask, state_below],
-				outputs_info = [
-					T.alloc(np_floatX(0.), n_samples, dim_proj),
-					T.alloc(np_floatX(0.), n_samples, dim_proj)
-					],
-				name=_p(prefix, '_layers'),
-				n_steps = nsteps
-			)
-	return rval[0]
-
-class LstmClassifier:
+class LstmLrClassifier:
 	def __init__(self):
 		pass
 
 	########################## Building Model ###################################
-	def init_params(options, Wemb = None):
+	def init_params(self, options, Wemb = None):
 		'''
 		initizalize params for every layer
 		'''
@@ -185,20 +140,82 @@ class LstmClassifier:
 
 		# Embedding and LSTM
 	   	params['Wemb'] = Wemb
-		params = get_layer(options['encoder'])[0](options, params, prefix = options['encoder'])
+		params = init_params_lstm(options, params, prefix = 'lstm')
 		
 		# logistic Regression
-		params['U'] = 0.01 * numpy.random.randn(options['dim_proj'], options['ydim']).astype(floatX)
-		params['b'] = numpy.zeros((options['ydim'],)).astype(floatX)
+		params['U'] = 0.01 * np.random.randn(options['dim_proj'], options['ydim']).astype(floatX)
+		params['b'] = np.zeros((options['ydim'],)).astype(floatX)
 
 		return params
 
-	def build_model(tparams, options):
+	def init_params_lstm(self, options, params, prefix='lstm'):
+		N = 4 # input/output/cell/forget gate
+
+		W = np.concatenate([ortho_weight(options['dim_proj']) for i in range(N)], axis=1)
+		params[_p(prefix, 'W')] = W
+
+		U = np.concatenate([ortho_weight(options['dim_proj']) for i in range(N)], axis=1)
+		params[_p(prefix, 'U')] = U
+
+		b = np.zeros((N * options['dim_proj'],))
+		params[_p(prefix, 'b')] = b.astype(floatX)
+
+		return params
+
+	def lstm_layer(self, tparams, state_below, options, prefix='lstm', mask=None):
+		nsteps = state_below.shape[0]
+		if state_below.ndim == 3:
+			n_samples = state_below.shape[1]
+		else:
+			n_samples = 1
+
+		assert mask is not None
+
+		def _slice(_x, n, dim):
+			if _x.ndim == 3:
+				return _x[:, :, n * dim:(n + 1) * dim]
+			return _x[:, n * dim:(n + 1) * dim]
+
+		def _step(m_, x_, h_, c_):
+			preact = T.dot(h_, tparams[_p(prefix, 'U')])
+			preact += x_
+
+			i = T.nnet.sigmoid(_slice(preact, 0, options['dim_proj']))
+			f = T.nnet.sigmoid(_slice(preact, 1, options['dim_proj']))
+			o = T.nnet.sigmoid(_slice(preact, 2, options['dim_proj']))
+			c = T.tanh(_slice(preact, 3, options['dim_proj']))
+
+			c = f * c_ + i * c
+			c = m_[:, None] * c + (1. - m_)[:, None] * c_
+
+			h = o * T.tanh(c)
+			h = m_[:, None] * h + (1. - m_)[:, None] * h_
+
+			return h, c
+
+		state_below = (T.dot(state_below, tparams[_p(prefix, 'W')]) +
+					   tparams[_p(prefix, 'b')])
+
+		dim_proj = options['dim_proj']
+		rval, updates = theano.scan(
+					_step,
+					sequences = [mask, state_below],
+					outputs_info = [
+						T.alloc(np_floatX(0.), n_samples, dim_proj),
+						T.alloc(np_floatX(0.), n_samples, dim_proj)
+						],
+					name=_p(prefix, '_layers'),
+					n_steps = nsteps
+				)
+		return rval[0]
+
+
+	def build_model(self, tparams, options):
 		trng = RandomStreams(SEED)
 
 		# Used for dropout.
 		# a shared variable, changed in the training process to control whether to use noise or not
-		use_noise = theano.shared(numpy_floatX(0.))
+		use_noise = theano.shared(np_floatX(0.))
 
 		# a matrix, whose shape is (n_timestep, n_samples) for theano.scan in training process
 		x = T.matrix('x', dtype='int64')
@@ -218,9 +235,7 @@ class LstmClassifier:
 							options['dim_proj']])
 
 		# the result of LSTM, a matrix of shape (n_timestep, n_samples, dim_proj)
-		proj = get_layer(options['encoder'])[1](tparams, emb, options,
-							prefix=options['encoder'],
-							mask=mask)
+		proj = self.lstm_layer(tparams, emb, options, prefix = 'lstm', mask = mask)
 
 		# mean pooling, a matrix of shape (n_samples, dim_proj)
 		if options['encoder'] == 'lstm':
@@ -238,11 +253,47 @@ class LstmClassifier:
 
 		off = 1e-8
 		if pred.dtype == 'float16':
-		off = 1e-6
+			off = 1e-6
 
 		cost = -T.log(pred[T.arange(n_samples), y] + off).mean()
 
 		return use_noise, x, mask, y, f_pred_prob, f_pred, cost
+
+	def adadelta(lr, tparams, grads, x, mask, y, cost):
+		'''
+		An adaptive learning rate optimizer
+		'''
+		zipped_grads = [theano.shared(p.get_value() * np_floatX(0.), name = '%s_grad' % k)
+						for k, p in tparams.iteritems()]
+		running_up2 = [theano.shared(p.get_value() * np_floatX(0.), name = '%s_rup2' % k)
+						for k, p in tparams.iteritems()]
+		running_grads2 = [theano.shared(p.get_value() * np_floatX(0.), name = '%s_rgrad2' % k)
+						for k, p in tparams.iteritems()]
+
+		zgup = [(zg, g) for zg, g in zip(zipped_grads, grads)]
+		rg2up = [(rg2, 0.95 * rg2 + 0.05 * (g ** 2))
+				 for rg2, g in zip(running_grads2, grads)]
+
+		f_grad_shared = theano.function(
+					[x, mask, y], cost,
+					updates = zgup + rg2up,
+					name = 'adadelta_f_grad_shared'
+				)
+
+		updir = [-T.sqrt(ru2 + 1e-6) / T.sqrt(rg2 + 1e-6) * zg
+				for zg, ru2, rg2 in zip(zipped_grads, running_up2, running_grads2)]
+		ru2up = [(ru2, 0.95 * ru2 + 0.05 * (ud ** 2))
+				for ru2, ud in zip(running_up2, updir)]
+		param_up = [(p, p + ud) for p, ud in zip(tparams.values(), updir)]
+
+		f_update = theano.function(
+					[lr], [],
+					updates = ru2up + param_up,
+					on_unused_input = 'ignore',
+					name = 'adadelta_f_update'
+				)
+
+		return f_grad_shared, f_update
 
 
 	########################## Classification ###################################
@@ -260,11 +311,11 @@ class LstmClassifier:
 		train_params = cPickle.load(open('%s.pkl'%(fname_model), 'r')) # why -1??
 		model_options.update(train_params)
 
-		params = lstmtool.init_params(model_options, None)
-		lstmtool.load_params(fname_model, params)
-		tparams = lstmtool.init_tparams(params)
+		params = self.init_params(model_options, None)
+		load_params(fname_model, params)
+		tparams = init_tparams(params)
 
-		use_noise, x, mask, y, f_pred_prob, f_pred, cost = lstmtool.build_model(tparams, model_options)
+		use_noise, x, mask, y, f_pred_prob, f_pred, cost = self.build_model(tparams, model_options)
 
 		self.f_pred = f_pred
 		self.f_pred_prob = f_pred_prob
@@ -284,13 +335,13 @@ class LstmClassifier:
 			logger.warning('not examined yet, please check')
 			return pred_probs[0]
 		else:
-			kf = lstmtool.get_minibatches_idx(len(seqs), batch_size)
+			kf = get_minibatches_idx(len(seqs), batch_size)
 		
 			#preds = []
 			pred_probs = []
 
 			for _, idx in kf:
-				pds = self.classify_batch([seqs[i] for i in idx])	
+				pds = self.classify_batch([seqs[i] for i in idx])
 				#preds.extend(ps)
 				pred_probs.extend(pds)
 
@@ -309,7 +360,7 @@ class LstmClassifier:
 		maxlen = np.max(lengths)
 
 		x = np.zeros((maxlen, n_samples)).astype('int64')
-		x_mask = np.zeros((maxlen, n_samples)).astype(theano.floatX)
+		x_mask = np.zeros((maxlen, n_samples)).astype(floatX)
 
 		for idx, s in enumerate(seqs):
 			x[:lengths[idx], idx] = s
@@ -339,7 +390,7 @@ class LstmClassifier:
 		lrate = 0.0001,
 		batch_size = 16,
 		valid_batch_size = 64,
-		optimizer = lstmtool.adadelta,
+		optimizer = self.adadelta,
 		noise_std = 0., 
 
 		# debug params
@@ -350,11 +401,11 @@ class LstmClassifier:
 		# building model
 		logger.info('building model...')
 
-		dim_proj = Wemb.shape[1] # numpy.ndarray expected
+		dim_proj = Wemb.shape[1] # np.ndarray expected
 
 		model_options = locals().copy()
 		model_options['dim_proj'] = dim_proj
-		model_options['encoder'] = 'lstm'
+		#model_options['encoder'] = 'lstm'
 
 		model_config = {
 			'ydim':ydim,
@@ -364,11 +415,11 @@ class LstmClassifier:
 		}
 		cPickle.dump(model_config, open('%s.pkl'%(fname_model), 'wb'), -1) # why -1??
 
-		params = lstmtool.init_params(model_options, Wemb)
+		params = self.init_params(model_options, Wemb)
 
 		if reload_model:
 			if os.path.exists(fname_model):
-				lstmtool.load_params(fname_model, params)
+				load_params(fname_model, params)
 			else:
 				logger.warning('model %s not found'%(fname_model))
 				return None
@@ -376,14 +427,14 @@ class LstmClassifier:
 			logger.warning('Wemb is missing for training LSTM')
 			return None
 		
-		tparams = lstmtool.init_tparams(params)
-		use_noise, x, mask, y, f_pred_prob, f_pred, cost = lstmtool.build_model(tparams, model_options)
+		tparams = init_tparams(params)
+		use_noise, x, mask, y, f_pred_prob, f_pred, cost = self.build_model(tparams, model_options)
 
 		# preparing functions for training
 		logger.info('preparing functions')
 
 		if decay_c > 0.:
-			decay_c = theano.shared(lstmtool.numpy_floatX(decay_c), name='decay_c')
+			decay_c = theano.shared(np_floatX(decay_c), name='decay_c')
 			weight_decay = 0.
 			weight_decay += (tparams['U'] ** 2).sum()
 			weight_decay *= decay_c
@@ -397,8 +448,8 @@ class LstmClassifier:
 		lr = T.scalar(name = 'lr')
 		f_grad_shared, f_update = optimizer(lr, tparams, grads, x, mask, y, cost)
 
-		kf_valid = lstmtool.get_minibatches_idx(len(valid[0]), valid_batch_size)
-		kf_test = lstmtool.get_minibatches_idx(len(test[0]), valid_batch_size)
+		kf_valid = get_minibatches_idx(len(valid[0]), valid_batch_size)
+		kf_test = get_minibatches_idx(len(test[0]), valid_batch_size)
 
 		if validFreq == None:
 			validFreq = len(train[0]) / batch_size
@@ -422,7 +473,7 @@ class LstmClassifier:
 			for eidx in xrange(max_epochs):
 				n_samples = 0
 				
-				kf = lstmtool.get_minibatches_idx(len(train[0]), batch_size, shuffle = True)
+				kf = get_minibatches_idx(len(train[0]), batch_size, shuffle = True)
 				
 				for _, train_index in kf:
 					uidx += 1
@@ -459,7 +510,7 @@ class LstmClassifier:
 						if best_p is not None:
 							params = best_p
 						else:
-							params = lstmtool.unzip(tparams)
+							params = unzip(tparams)
 					
 						np.savez(fname_model, history_errs = history_errs, **params)
 
@@ -472,14 +523,13 @@ class LstmClassifier:
 						logger.info('Validation ....')
 
 						# not necessary	
-						train_err = lstmtool.pred_error(f_pred, self.prepare_data, train, kf)
-						
-						valid_err = lstmtool.pred_error(f_pred, self.prepare_data, valid, kf_valid)
-						test_err = lstmtool.pred_error(f_pred, self.prepare_data, test, kf_test)
+						train_err = pred_error(f_pred, self.prepare_data, train, kf)
+						valid_err = pred_error(f_pred, self.prepare_data, valid, kf_valid)
+						test_err = pred_error(f_pred, self.prepare_data, test, kf_test)
 
 						history_errs.append([valid_err, test_err])
 						if (uidx == 0 or valid_err <= np.array(history_errs)[:, 0].min()):
-							best_p = lstmtool.unzip(tparams)
+							best_p = unzip(tparams)
 							bad_count = 0
 						
 						logger.info('prediction error: train %f valid %f test %f'%(
@@ -503,16 +553,16 @@ class LstmClassifier:
 		end_time = time.time()
 
 		if best_p is not None:
-			lstmtool.zipp(best_p, tparams)
+			zipp(best_p, tparams)
 		else:
-			best_p = lstmtool.unzip(tparams)
+			best_p = unzip(tparams)
 
 		use_noise.set_value(0.)
 		
-		kf_train = lstmtool.get_minibatches_idx(len(train[0]), batch_size)
-		train_err = lstmtool.pred_error(f_pred, self.prepare_data, train, kf_train)
-		valid_err = lstmtool.pred_error(f_pred, self.prepare_data, valid, kf_valid)
-		test_err = lstmtool.pred_error(f_pred, self.prepare_data, test, kf_test)
+		kf_train = get_minibatches_idx(len(train[0]), batch_size)
+		train_err = pred_error(f_pred, self.prepare_data, train, kf_train)
+		valid_err = pred_error(f_pred, self.prepare_data, valid, kf_valid)
+		test_err = pred_error(f_pred, self.prepare_data, test, kf_test)
  
 		logger.info('prediction error: train %f valid %f test %f'%(
 				train_err, valid_err, test_err)
@@ -532,121 +582,35 @@ class LstmClassifier:
 		self.f_pred = f_pred
 		self.tparams = tparams
 
-		return train_err, valid_err, test_err, end_time - start_time
+		return train_err, valid_err, test_err
 
-'''
-from tfcoder import TfCoder
-
-def randWemb(n_words, dim_proj):
-	randn = np.random.rand(n_words, dim_proj)
-	return (0.01 * randn).astype(theano.floatX)
-
-def main():
-	import cPickle
-	from const import N_EMO	
-
-	########################### OptionParser ################################
-	optparser = OptionParser()
+def test():	
+	ydim = 2
+	xdim = 3
+	n_word = 20
+	n_length = 5
 	
-	# necessary
-	optparser.add_option('-d', '--dim_proj', action='store', type='int', dest='dim_proj') #, default = 128
-	optparser.add_option('-p', '--prefix', action='store', type='str', dest='prefix') #, default = 128
+	Wemb = np.random.random((n_word, xdim))
 
-	# debug
-	optparser.add_option('-y', '--ydim', action='store', type='int', dest='ydim', default = N_EMO)
-	optparser.add_option('-n', '--n_samples', action='store', type='int', dest='n_samples', default = None)
+	def random_xy(n_samples):
+		x = [[np.random.randint(n_word) for j in range(n_length)] for i in range(n_samples)]
+		y = [np.random.randint(ydim) for j in range(n_samples)]
 
-	# especially for gpu
-	optparser.add_option('-b', '--batch_size', action='store', type='int', dest='batch_size', default = 16)
-	optparser.add_option('-r', '--resume', action='store_true', dest='resume', default = False)
+		return (x, y)
+	
+	train = random_xy(2000)
+	valid = random_xy(300)
+	test = random_xy(100)
+	dataset = (train, valid, test)
 
-	opts, args = optparser.parse_args()
-
-	########################## LSTM Training ####################################
-	from const import PKL_TFCODER
-	coder = cPickle.load(open(PKL_TFCODER, 'r'))
-	n_emo = opts.ydim
-	datalen = opts.n_samples
-	dim_proj = opts.dim_proj
-
-	fname_model = 'output/%s_model.npz'%(opts.prefix)
-	fname_result = 'output/%s_test.pkl'%(opts.prefix)
-	fname_valid_prefix = 'output/%s'%(opts.prefix)
-
-	Wemb = randWemb(coder.n_code(), dim_proj)
-
-	import baseunidatica as unidatica
-	dataset = unidatica.load(n_emo, datalen)
-
-	lstm = LstmClassifier()
-	res = lstm.train(
+	clf = LstmLrClassifier()
+	res = clf.train(
 			dataset = dataset,
 			Wemb = Wemb,
-			ydim = n_emo,
-
-			fname_model = fname_model,
-			reload_model = opts.resume,
-
-			batch_size = opts.batch_size,
-			valid_batch_size = opts.batch_size,
+			ydim = ydim,
+			
+			fname_model = 'output/lstmlr_model.npz'
 		)
-	######################### Test #############################################
-	test_x, test_y = dataset[2]
-	preds_prob = lstm.classify(test_x)
-	cPickle.dump((test_y, preds_prob), open(fname_result, 'w'))
-
-	######################### Graph ############################################
-	import validatica
-	validatica.report(test_y, preds_prob, fname_valid_prefix)
-
-
-def valid(n_emo, datalen, fname_model, fname_result, fname_valid_prefix):
-	import cPickle
-	import tfcoder	
-	from const import PKL_TFCODER, N_EMO
-
-	#coder = cPickle.load(open(PKL_TFCODER, 'r'))
-	#n_emo = 2 #N_EMO
-
-	lstm = LstmClassifier()
-	lstm.load(
-			#ydim = n_emo,
-			#n_words = coder.n_code(),
-			fname_model = fname_model,
-		)
-
-	import baseunidatica as unidatica
-	dataset = unidatica.load(n_emo, datalen)
-	test_x, test_y = dataset[2]
-
-	preds_prob = lstm.classify(test_x)
-	cPickle.dump((test_y, preds_prob), open(fname_result, 'w'))
-
-	import validatica
-	validatica.report(test_y, preds_prob, fname_valid_prefix)
-
-
-def main_valid():
-	optparser = OptionParser()
-	optparser.add_option('-p', '--prefix', action='store', type='str', dest='prefix')
-	optparser.add_option('-n', '--n_samples', action='store', type='int', dest='n_samples', default = None)
-	optparser.add_option('-y', '--ydim', action='store', type='int', dest='ydim') #, default=N_EMO
-
-	optparser.add_option('-d', '--dim_proj', action='store', type='int') #, default = 128
-	opts, args = optparser.parse_args()
-
-	prefix = opts.prefix
-	n_emo = opts.ydim
-	datalen = opts.n_samples
-
-	fname_model = 'output/%s_model.npz'%(prefix)
-	fname_result = 'output/%s_test.pkl'%(prefix + '_valid')
-	fname_valid_prefix = 'output/%s'%(prefix + '_valid')
-
-	valid(n_emo, datalen, fname_model, fname_result, fname_valid_prefix)
-'''
 
 if __name__ == '__main__':
-	main()
-	#main_valid()
-
+	test()
